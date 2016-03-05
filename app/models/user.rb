9342064c8b869ecd4e30c1c8ad0aeae45df88ea7
Nikhil Hira <nikhil.hira@ilabsolutions.com>
2016-03-05 13:55:48 -0800

    add Account model, add valiation for email uniqueness to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,26 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  validate :email_is_unique, on: :create
+
+  after_create :create_account
 
   def confirmation_required?
     false
+  end
+
+  private
+
+  # email should be unique on the Account model
+  def email_is_unique
+    if Account.find_by(email: email).present?
+      errors.add(email: " already has an account.")
+    end
+  end
+
+  def create_account
+    account = Account.new(email: email)
+    account.save!
   end
 
 end
