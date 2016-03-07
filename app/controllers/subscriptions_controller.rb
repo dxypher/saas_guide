41ddb09ad0d0c8861908fa6245bb74b9936e6b17
Nikhil Hira <nikhil.hira@ilabsolutions.com>
2016-03-06 16:23:32 -0800

    allow customer to upgrade or downgrade their subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,6 +2,15 @@ class SubscriptionsController < ApplicationController
 
   before_action :authenticate_user!
 
+  def index
+    @account = Account.find_by(email: current_user.email)
+  end
+
+  def edit
+    @account = Account.find(params[:id])
+    @plans = Plan.all
+  end
+
   def new
     @plans = Plan.all
   end
@@ -17,32 +26,60 @@ class SubscriptionsController < ApplicationController
     token = params[:stripeToken]
     plan  = params[:plan][:stripe_id]
     email = current_user.email #assumes user is logged in to subscribe
+    current_account = Account.find_by(email: email)
+    customer_id = current_account.customer_id
+    current_plan = current_account.stripe_plan_id
 
-    # Create a Customer
-    customer = Stripe::Customer.create(
+    if customer_id.nil?
+      # Create a Customer
+      @customer = Stripe::Customer.create(
       :source => token,
       :plan => plan,
       :email => email
-    )
+      )
+      subscriptions = @customer.subscriptions
+      @subscription = subscriptions.data[0]
+    else
+      @customer = Stripe::Customer.retrieve(customer_id)
+      @subscription = create_or_update_subscription(@customer, current_plan, plan)
+    end
 
-    subscriptions = customer.subscriptions
-    subscription = subscriptions.data[0]
     # This is a unix timestamp
-    current_period_end = subscription.current_period_end
+    current_period_end = @subscription.current_period_end
     # convert to datetime
     active_until = Time.at(current_period_end).to_datetime
-    # Customer created valid subscription
-    # create associated Account record
-    account = Account.find_by(email: email)
-    account.stripe_plan_id = plan
-    account.customer_id = customer.id
-    account.active_until = active_until
-    account.save!
+
+    save_account_details( current_account, plan, @customer.id, active_until )
 
     redirect_to :root, notice: 'Succesfully subscibed!'
 
   rescue => e
-    redirect_to :new_subscription, flash: {error: e.message}
+    redirect_to :back, flash: {error: e.message}
+  end
+
+  private
+
+  def create_or_update_subscription(customer, current_plan, new_plan)
+    subscriptions = customer.subscriptions
+    current_subscription = subscriptions.data[0]
+
+    if current_subscription.blank?
+      subscription = customer.subscriptions.create(plan: new_plan)
+    else
+      current_subscription.plan = new_plan
+      subscription = current_subscription.save
+    end
+
+    subscription
+  end
+
+  def save_account_details( current_account, plan, customer_id, active_until )
+    # Customer created valid subscription
+    # create associated Account record
+    current_account.stripe_plan_id = plan
+    current_account.customer_id = customer_id
+    current_account.active_until = active_until
+    current_account.save!
   end
 
 end
