d7e828e936422c8e840dd22b208d42ea785a4564
Nikhil Hira <nikhil.hira@ilabsolutions.com>
2016-03-09 18:53:37 -0800

    add error print statement

--- lib/rescue_apartment_middleware.rb
+++ lib/rescue_apartment_middleware.rb
@@ -3,7 +3,9 @@ module RescueApartmentMiddleware
   def call(*args)
     begin
       super
-    rescue Apartment::TenantNotFound
+    rescue Apartment::TenantNotFound => e
+      ap 'Apartment::TenantNotFound'
+      ap e.message
       return [404, { "Content-Type" => "text/html" }, ["#{ File.read(Rails.root.to_s + '/public/404.html') }"]]
     end
   end
